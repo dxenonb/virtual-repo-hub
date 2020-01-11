363148ffd7cfa7afda8e88990c9bbdc92fccf924
dxb <dr.dxb.me@gmail.com>
2020-01-11 15:39:07 -0500

    Add branches to status

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use git2::{Repository, RepositoryState};
 
 use std::env;
+use std::collections::HashMap;
+
+#[cfg(test)]
+mod test;
 
 #[derive(Debug)]
 struct RepoStatus {
@@ -11,7 +15,7 @@ struct RepoStatus {
     clean_state: bool,
     stashes: usize,
     remotes: Vec<Remote>,
-    // branches: HashMap<String, BranchStatus>,
+    branches: HashMap<String, BranchStatus>,
 }
 
 #[derive(Debug)]
@@ -19,18 +23,45 @@ struct Remote {
     name: String,
 }
 
-/*
-struct BranchStatus {
-    tracking_status: TrackingStatus,
-    merged_in_tracking: bool,
+#[derive(Debug)]
+enum BranchStatus {
+    TrackingBranch(TrackingStatus),
+    LocalBranch {
+        merged_in_remote: bool,
+    },
 }
 
+impl BranchStatus {
+    fn new_tracking_branch(tracking_status: TrackingStatus) -> Self {
+        BranchStatus::TrackingBranch(tracking_status)
+    }
+
+    fn new_local_branch(merged_in_remote: bool) -> Self {
+        BranchStatus::LocalBranch { merged_in_remote }
+    }
+
+
+    fn merged_in_upstream(&self) -> bool {
+        let tracking_status = match self {
+            BranchStatus::TrackingBranch(status) => status,
+            BranchStatus::LocalBranch { merged_in_remote } => return *merged_in_remote,
+        };
+
+        use TrackingStatus::*;
+        match tracking_status {
+            Diverged | Ahead => false,
+            Behind | Current => true,
+        }
+    }
+}
+
+#[derive(Debug)]
 enum TrackingStatus {
     Diverged,
     Ahead,
     Behind,
+    Current,
 }
-*/
 
 fn main() -> Result<(), i32> {
     println!("Checking if repo is safely backed up...");
@@ -82,6 +113,7 @@ fn get_status(repo: &mut Repository) -> Result<RepoStatus, git2::Error> {
             clean_status: true,
             clean_state: true,
             stashes: 0,
+            branches: HashMap::new(),
         });
     }
 
@@ -106,11 +138,109 @@ fn get_status(repo: &mut Repository) -> Result<RepoStatus, git2::Error> {
         true
     })?;
 
+    let mut local_only_branches = Vec::new();
+    let mut branches = HashMap::new();
+
+    let mut branch_iter = repo.branches(Some(git2::BranchType::Local))?;
+    for branch in branch_iter {
+        let (branch, _) = branch?;
+        let tracking_status = match branch.upstream() {
+            Ok(upstream) => {
+                // get the merge base
+                let upstream_commit = upstream.get().peel_to_commit()?.id();
+                let branch_commit = branch.get().peel_to_commit()?.id();
+
+                if upstream_commit == branch_commit {
+                    TrackingStatus::Current
+                } else {
+                    // TODO: The error codes aren't document for this...
+                    let ancestor = repo.merge_base(branch_commit, upstream_commit)
+                        .expect("merge base error");
+                    if ancestor == branch_commit {
+                        // if the merge base is branch, then branch is behind the upstream
+                        TrackingStatus::Behind
+                    } else if ancestor == upstream_commit {
+                        // if the merge base is the upstream, then branch is ahead
+                        TrackingStatus::Ahead
+                    } else {
+                        // if it is neither, the branches have diverged
+                        TrackingStatus::Diverged
+                    }
+                }
+            },
+            Err(err) => {
+                if err.code() != git2::ErrorCode::NotFound {
+                    return Err(err);
+                }
+                // add the branch to an auxillary list to be checked
+                local_only_branches.push(branch);
+                continue;
+            }
+        };
+
+        let name = branch.name()?
+            .unwrap_or_else(|| "[non utf-8]")
+            .to_string();
+
+        let status = BranchStatus::new_tracking_branch(tracking_status);
+        branches.insert(name, status);
+    }
+
+    // loop over remote branches and check if the auxillary branches are merged
+    branch_iter = repo.branches(Some(git2::BranchType::Local))?;
+    for remote_branch in branch_iter {
+        let (remote_branch, _) = remote_branch?;
+        let remote_commit = remote_branch.get().peel_to_commit()?.id();
+        let mut iter_err = None;
+        local_only_branches.retain(|branch| {
+            let commit = match branch.get().peel_to_commit() {
+                Ok(commit) => commit.id(),
+                Err(err) => {
+                    iter_err = Some(err);
+                    return false;
+                },
+            };
+            let ancestor = repo.merge_base(commit, remote_commit)
+                .expect("merge base error");
+
+            let result = ancestor == commit;
+            if result {
+                let name = match branch.name() {
+                    Ok(branch) => branch.unwrap_or_else(|| "[non utf-8]")
+                        .to_string(),
+                    Err(err) => {
+                        iter_err = Some(err);
+                        return false;
+                    },
+                };
+                branches.insert(name, BranchStatus::new_local_branch(true));
+            }
+
+            result
+        });
+
+        if let Some(err) = iter_err {
+            return Err(err);
+        }
+
+        if local_only_branches.is_empty() {
+            break;
+        }
+    }
+
+    for local_branch in local_only_branches {
+        let name = local_branch.name()?
+            .unwrap_or_else(|| "[non utf-8]")
+            .to_string();
+        branches.insert(name, BranchStatus::new_local_branch(false));
+    }
+
     Ok(RepoStatus {
         bare: false,
         clean_status,
         clean_state,
         stashes,
         remotes,
+        branches,
     })
 }
